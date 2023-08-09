6807099b09c3d10472cc957c3cce15bf140751fa
Teerthesh <teerthesh706@gmail.com>
2023-08-09 17:15:28 +0530

    8Aug2023

---
+++ 8Aug.js
@@ -0,0 +1,113 @@
+console.log("welcome in javascript")
+//abv property is use to print in js console
+
+document.write("hello SAGE")
+// abv property is use to write in the webpage
+
+// var z = 10; var y=25 ;
+// console.log(z)
+// console.log(z+y)
+
+// var can be redeclare and reassign
+// let can not be redeclare but can be reassign
+// const can neither be redeclare nor reassign
+
+// alert("thisMYcomputer") to show the alert when the site
+
+// {5=='5'} only check the digit
+// {5==='5'} it will check the digit as well as the datatype
+// prompt("Enter your message") this is use to take input from user
+
+// program to validate the eligibility of user for vote:
+
+// let age = prompt("Enter your age:")
+// if(age>=18){
+//     console.log("You are eligible to vote")
+// }
+// else if(age>=10){
+//     console.log("You are eligible for baby vote")
+// }
+// else{
+//     console.log("You are not eligible to vote")
+// }
+
+// program to check which number is greater among two number:
+
+// let a =10
+// let b = 20
+// if(a<b){
+//     console.log("b is greater")
+// }
+// else{
+//     console.log('a is greater')
+// }
+
+// the prompt keyword automatically take input in string datatype so for integer we need to typecast the input in number:
+
+// program to check which no. is greatest among three numbers:
+
+// let g= Number(prompt("Enter the first value:"))
+// let h= Number(prompt("Enter the second value:"))
+// let i= Number(prompt("Enter the third value:"))
+// if(g>h && g>i){
+//     console.log("First value is greater")
+// }
+// else if (h>g && h>i){
+//     console.log("Second value is greater")
+// }
+// else{
+//     console.log ("Third value is greater")
+// }
+
+
+// program to print week days by pressing no. keys:
+// let n= Number(prompt("Enter the Number"))
+// switch (n) {
+//         case 1:
+//         console.log("monday")
+//         break;
+//         case 2:
+//             console.log("tuesday")
+//         break;
+//         case 3:
+//             console.log("wednesday")
+//             break;
+            
+//             case 4:
+//                 console.log("thursday")
+//                 break;
+//                 case 5:
+//                     console.log("friday")
+//                     break;
+//                     case 6:
+//                         console.log("saturday")
+//                         break;
+//                         case 7:
+//                             console.log("sunday")
+//                             break;
+
+//     default:
+//         console.log("out of range")
+//         break;
+// }
+
+
+// program to check weather the no. is even or odd:
+// let n= Number(prompt("Enter the number"))
+// if (n%2==0){
+//     console.log("Number is Even")
+// }
+// else{
+//     console.log("Number is Odd")
+// }
+
+let n = prompt("Enter you Input")
+if('A'>=n>='Z' || 'a'>=n>='z'){
+    console.log("This is alphabet")
+}
+else if(0>=n>=0){
+    console.log("this is number")
+}
+else{
+    console.log("this is special character")
+}
